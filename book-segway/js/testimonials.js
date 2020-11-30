337914d2d8142abc06659c2e94c3a9632f516ff2
reneeipro <44815655+reneeipro@users.noreply.github.com>
2020-11-30 15:51:15 -0800

    Add files via upload

---
+++ book-segway/js/testimonials.js
@@ -0,0 +1,98 @@
+const testimonials = [
+    {
+      name: "Mahidhar Nyayapati",
+      location: "India",
+      src: "https://player.vimeo.com/video/393508094",
+      id: "393508094"
+    },
+    {
+      name: "Lynda Bathory",
+      location: "Canada",
+      src: "https://player.vimeo.com/video/393507886",
+      id: "393507886"
+    },
+    {
+      name: "Watel Guerin",
+      location: "United States",
+      src: "https://player.vimeo.com/video/393508796",
+      id: "393508796"
+    },
+    {
+      name: "Andres Monsalvo",
+      location: "Canada",
+      src: "https://player.vimeo.com/video/394055553",
+      id: "394055553"
+    },
+    {
+      name: "Asif Nazeer",
+      location: "Sri Lanka",
+      src: "https://player.vimeo.com/video/394059097",
+      id: "394059097"
+    },
+    {
+      name: "Amberlyn Murray",
+      location: "United States",
+      src: "https://player.vimeo.com/video/394059052",
+      id: "394059052"
+    },
+    {
+      name: "Kishor Akshinthala",
+      location: "United States",
+      src: "https://player.vimeo.com/video/394059109",
+      id: "394059109"
+    },
+    {
+      name: "Daniel Jimenez Peralta",
+      location: "Spain",
+      src: "https://player.vimeo.com/video/395031168",
+      id: "395031168"
+    },
+    {
+      name: "Freddy Tay",
+      location: "Singapore",
+      src: "https://player.vimeo.com/video/395341024",
+      id: "395341024"
+    },
+    {
+      name: "Yarob Al-Taay",
+      location: "Scotland",
+      src: "https://player.vimeo.com/video/395337010",
+      id: "395337010"
+    },
+    {
+      name: "Melvin Johnson",
+      location: "United States",
+      src: "https://player.vimeo.com/video/405237340",
+      id: "405237340"
+    },
+    {
+      name: "Duke Nguyen",
+      location: "Canada",
+      src: "https://player.vimeo.com/video/405238972",
+      id: "405238972"
+    },
+    {
+      name: "Jocelyn Canaya",
+      location: "Canada",
+      src: "https://player.vimeo.com/video/405239030",
+      id: "405239030"
+    },
+    {
+      name: "Kentia Tucker",
+      location: "United States",
+      src: "https://player.vimeo.com/video/405239042",
+      id: "405239042"
+    },
+    {
+      name: "Vivian Flores",
+      location: "United States",
+      src: "https://player.vimeo.com/video/405239107",
+      id: "405239107"
+    },
+    {
+      name: "Samuel Pierrot",
+      location: "United States",
+      src: "https://player.vimeo.com/video/405239071",
+      id: "405239071"
+    },
+  ]
